8f1681ec15f4029eeb692410c107bf26f8ce7112
justshurik <zaq01@ya.ru>
2016-09-10 19:14:04 +0300

    My project

---
+++ Pro1/app/src/main/java/ru/home/pro1/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.home.pro1.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.home.pro1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
